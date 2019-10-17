d308018016f7e236a4949ccec7e284f558f18669
Setillo <setillofm@gmail.com>
2019-10-17 19:44:03 +0200

    Update YesNoDialogTest.java
    Removidos import innecesarios

--- src/test/java/santaTecla/utils/YesNoDialogTest.java
+++ src/test/java/santaTecla/utils/YesNoDialogTest.java
@@ -1,6 +1,5 @@
 package santaTecla.utils;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -8,8 +7,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.Mockito.anyString;
-import static org.mockito.Mockito.when;
 
 class YesNoDialogTest {
 
